1f97188de5560e16c9dc74c07594447d1119a87d
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-08-21 10:56:44 +0200

    Initial likely working MetadataRepo implementation
    TODO:
    - separate commit for create_repository
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- taf/tuf/repository.py
+++ taf/tuf/repository.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 from datetime import datetime, timedelta, timezone
 from typing import Dict, List
 
-from securesystemslib.signer import CryptoSigner, Signer
+from securesystemslib.signer import Signer
 
 from tuf.api.metadata import (
     Metadata,
@@ -21,35 +21,24 @@ from tuf.repository import Repository
 
 logger = logging.getLogger(__name__)
 
-_signed_init = {
-    "root": Root,
-    "snapshot": Snapshot,
-    "targets": Targets,
-    "timestamp": Timestamp,
-}
-TARGETS_DIRECTORY_NAME = "targets"
 METADATA_DIRECTORY_NAME = "metadata"
 
 
 class MetadataRepository(Repository):
-    """TUF metadata repository
-
-    This repository keeps the metadata for all versions of all roles in memory.
-    It also keeps all target content in memory.
-
+    """TUF metadata repository.
 
     Attributes:
         signer_cache: All signers available to the repository. Keys are role
             names, values are lists of signers
     """
 
+    # TODO: define per-role expiry
     expiry_period = timedelta(days=1)
 
     def __init__(self, path: str) -> None:
 
-        self.path = Path(path)
-
         self.signer_cache: Dict[str, List[Signer]] = defaultdict(list)
+        self._path = Path(path)
 
         # current snapshot version cache
         self._snapshot_info = MetaFile(1)
@@ -57,12 +46,8 @@ class MetadataRepository(Repository):
         self._targets_infos: Dict[str, MetaFile] = defaultdict(lambda: MetaFile(1))
 
     @property
-    def targets_path(self) -> Path:
-        return self.path / TARGETS_DIRECTORY_NAME
-
-    @property
     def metadata_path(self) -> Path:
-        return self.path / METADATA_DIRECTORY_NAME
+        return self._path / METADATA_DIRECTORY_NAME
 
     @property
     def targets_infos(self) -> Dict[str, MetaFile]:
@@ -73,28 +58,8 @@ class MetadataRepository(Repository):
         return self._snapshot_info
 
     def open(self, role: str) -> Metadata:
-        """Return current Metadata for role from 'storage' (or create a new one)"""
-        if role == "root":
-            # TODO: this doesn't look so nice
-            paths = list(self.metadata_path.glob("*.root.json"))
-            md_path = paths[0] if paths else None
-
-        else:
-            md_path = self.metadata_path / f"{role}.json"
-
-        if md_path and md_path.exists():
-            return Metadata.from_file(md_path)
-
-        signed_init = _signed_init.get(role, Targets)
-        md = Metadata(signed_init())
-
-        # this makes version bumping in close() simpler
-        md.signed.version = 0
-
-        if isinstance(md, Root):
-            md.consistent_snapshot = False
-
-        return md
+        """Return current Metadata for role from disk."""
+        return Metadata.from_file(self.metadata_path / f"{role}.json")
 
     def close(self, role: str, md: Metadata) -> None:
         """Store a version of metadata. Handle version bumps, expiry, signing"""
@@ -105,20 +70,41 @@ class MetadataRepository(Repository):
         for signer in self.signer_cache[role]:
             md.sign(signer, append=True)
 
-        # store new metadata version, update version caches
-        self.role_cache[role].append(md)
+        fname = f"{role}.json"
+
+        # Default `do_snapshot` and `do_timestamp` implementations need
+        # the current metadata versions
         if role == "snapshot":
             self._snapshot_info.version = md.signed.version
-        elif role != "timestamp":
-            self._targets_infos[f"{role}.json"].version = md.signed.version
+        elif role != "timestamp":  # role in [root, targets, <delegated targets>]
+            self._targets_infos[fname].version = md.signed.version
+
+        md.to_file(self.metadata_path / fname)
+        # Store version-prefixed copy for root
+        if role == "root":
+            md.to_file(self.metadata_path / f"{md.signed.version}.{fname}")
 
     def create_repository(self):
-        with self.edit_root() as root:
-            for role in ["root", "timestamp", "snapshot", "targets"]:
-                signer = CryptoSigner.generate_ecdsa()
-                self.signer_cache[role].append(signer)
+        """Create new metadata repository.
+
+        1. Create metadata subdir (fail, if exists)
+        2. Create initial versions of top-level metadata
+        3. Perform top-level delegation using keys from signer_cache.
+
+        TODO: Should allow set threshold? Or default to 1? Or len(signers)?
+        TODO: Should allow add target files?
+
+        """
+        self.metadata_path.mkdir()
+
+        root = Root(consistent_snapshot=False)
+        for role in ["root", "timestamp", "snapshot", "targets"]:
+            for signer in self.signer_cache[role]:
                 root.add_key(signer.public_key, role)
 
-        for role in ["root", "timestamp", "snapshot", "targets"]:
-            with self.edit(role):
-                pass
+        for signed in [root, Timestamp(), Snapshot(), Targets()]:
+            signed.version = 0  # bumps to initial valid verison 1 in `close`
+            self.close(signed.type, Metadata(signed))
+
+        self.do_snapshot()
+        self.do_timestamp()
