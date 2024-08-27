0b5fc0cb7e3c611a46af1b207556fbc18e61d767
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-08-27 14:44:33 +0200

    Convert hsm test setup from unittest to pytest
    ```
    PYKCS11LIB=$(brew --prefix softhsm)/lib/softhsm/libsofthsm2.so \
    PYTEST_DISABLE_PLUGIN_AUTOLOAD=1 \
     pytest --noconftest taf/tests/tuf/ taf/tests/tuf/test_yk.py -s -v
    ```
    
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- taf/tests/tuf/test_yk.py
+++ taf/tests/tuf/test_yk.py
@@ -1,27 +1,100 @@
 """Test HSMSigner"""
 
 import os
-import shutil
-import tempfile
-import unittest
+import pytest
 
 from asn1crypto.keys import (
     ECDomainParameters,
     NamedCurve,
 )
-from cryptography.hazmat.primitives.asymmetric.ec import SECP256R1, SECP384R1
+from cryptography.hazmat.primitives.asymmetric.ec import SECP256R1
 from PyKCS11 import PyKCS11
 
 from securesystemslib.exceptions import UnverifiedSignatureError
-from securesystemslib.signer import HSMSigner, Signer
+from securesystemslib.signer import HSMSigner
 from securesystemslib.signer._hsm_signer import PYKCS11LIB
 
 _HSM_KEYID = 1
 _HSM_USER_PIN = "123456"
 
 
-@unittest.skipUnless(os.environ.get("PYKCS11LIB"), "set PYKCS11LIB to SoftHSM lib path")
-class TestHSM(unittest.TestCase):
+def _generate_key_pair(session, keyid, curve):
+    "Create ecdsa key pair on hsm"
+    params = ECDomainParameters(name="named", value=NamedCurve(curve.name)).dump()
+
+    public_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
+        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_WRAP, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+        (PyKCS11.CKA_EC_PARAMS, params),
+        (PyKCS11.CKA_LABEL, curve.name),
+        (PyKCS11.CKA_ID, (keyid,)),
+    ]
+    private_template = [
+        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
+        (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
+        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_DECRYPT, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
+        (PyKCS11.CKA_UNWRAP, PyKCS11.CK_FALSE),
+        (PyKCS11.CKA_LABEL, curve.name),
+        (PyKCS11.CKA_ID, (keyid,)),
+    ]
+
+    session.generateKeyPair(
+        public_template,
+        private_template,
+        mecha=PyKCS11.MechanismECGENERATEKEYPAIR,
+    )
+
+
+@pytest.fixture
+def test_hsm(tmp_path):
+    """Initialize SoftHSM token and generate ecdsa test keys"""
+    so_pin = "abcd"
+    token_label = "Test SoftHSM"
+
+    # Configure SoftHSM to create test token in temporary test directory
+    original_cwd = os.getcwd()
+    os.chdir(tmp_path)
+
+    with open("softhsm2.conf", "w", encoding="utf-8") as f:
+        f.write("directories.tokendir = " + str(tmp_path))
+    os.environ["SOFTHSM2_CONF"] = str(tmp_path / "softhsm2.conf")
+
+    # Only load shared library after above config
+    lib = PYKCS11LIB()
+    slot = lib.getSlotList(tokenPresent=True)[0]
+    lib.initToken(slot, so_pin, token_label)
+
+    session = PYKCS11LIB().openSession(slot, PyKCS11.CKF_RW_SESSION)
+    session.login(so_pin, PyKCS11.CKU_SO)
+    session.initPin(_HSM_USER_PIN)
+    session.logout()
+
+    session.login(_HSM_USER_PIN)
+
+    # Generate test ecdsa key pairs for curves secp256r1 and secp384r1 on test token
+    _generate_key_pair(session, _HSM_KEYID, SECP256R1)
+
+    session.logout()
+    session.closeSession()
+
+    yield
+
+    os.chdir(original_cwd)
+    del os.environ["SOFTHSM2_CONF"]
+
+
+@pytest.mark.skipif(
+    not os.environ.get("PYKCS11LIB"), reason="set PYKCS11LIB to SoftHSM lib path"
+)
+class TestHSM:
     """Test HSMSigner with SoftHSM
 
     Requirements:
@@ -31,93 +104,13 @@ class TestHSM(unittest.TestCase):
     See .github/workflows/hsm.yml for how this can be done on Linux, macOS and Windows.
     """
 
-    @staticmethod
-    def _generate_key_pair(session, keyid, curve):
-        "Create ecdsa key pair on hsm"
-        params = ECDomainParameters(name="named", value=NamedCurve(curve.name)).dump()
-
-        public_template = [
-            (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
-            (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
-            (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-            (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_FALSE),
-            (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
-            (PyKCS11.CKA_WRAP, PyKCS11.CK_FALSE),
-            (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-            (PyKCS11.CKA_EC_PARAMS, params),
-            (PyKCS11.CKA_LABEL, curve.name),
-            (PyKCS11.CKA_ID, (keyid,)),
-        ]
-        private_template = [
-            (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
-            (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_ECDSA),
-            (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-            (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
-            (PyKCS11.CKA_DECRYPT, PyKCS11.CK_FALSE),
-            (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
-            (PyKCS11.CKA_UNWRAP, PyKCS11.CK_FALSE),
-            (PyKCS11.CKA_LABEL, curve.name),
-            (PyKCS11.CKA_ID, (keyid,)),
-        ]
-
-        session.generateKeyPair(
-            public_template,
-            private_template,
-            mecha=PyKCS11.MechanismECGENERATEKEYPAIR,
-        )
-
-    @classmethod
-    def setUpClass(cls):
-        """Initialize SoftHSM token and generate ecdsa test keys"""
-        so_pin = "abcd"
-        token_label = "Test SoftHSM"
-
-        # Configure SoftHSM to create test token in temporary test directory
-        cls.original_cwd = os.getcwd()
-        cls.test_dir = os.path.realpath(tempfile.mkdtemp())
-        os.chdir(cls.test_dir)
-
-        with open("softhsm2.conf", "w", encoding="utf-8") as f:
-            f.write("directories.tokendir = " + os.path.join(cls.test_dir, ""))
-        os.environ["SOFTHSM2_CONF"] = os.path.join(cls.test_dir, "softhsm2.conf")
-
-        # Only load shared library after above config
-        lib = PYKCS11LIB()
-        slot = lib.getSlotList(tokenPresent=True)[0]
-        lib.initToken(slot, so_pin, token_label)
-
-        tokeninfo = lib.getTokenInfo(slot)
-        session = PYKCS11LIB().openSession(slot, PyKCS11.CKF_RW_SESSION)
-        session.login(so_pin, PyKCS11.CKU_SO)
-        session.initPin(_HSM_USER_PIN)
-        session.logout()
-
-        session.login(_HSM_USER_PIN)
-
-        # Generate test ecdsa key pairs for curves secp256r1 and secp384r1 on test token
-        cls._generate_key_pair(session, _HSM_KEYID, SECP256R1)
-
-        session.logout()
-        session.closeSession()
-
-    @classmethod
-    def tearDownClass(cls):
-        os.chdir(cls.original_cwd)
-        shutil.rmtree(cls.test_dir)
-        del os.environ["SOFTHSM2_CONF"]
-
-    def test_hsm(self):
+    def test_hsm(self, test_hsm):
         """Test HSM key export and signing."""
 
-        _, key = HSMSigner.import_(_HSM_KEYID, None)
+        _, key = HSMSigner.import_(_HSM_KEYID)
         signer = HSMSigner(_HSM_KEYID, {}, key, lambda sec: _HSM_USER_PIN)
         sig = signer.sign(b"DATA")
         key.verify_signature(sig, b"DATA")
 
-        with self.assertRaises(UnverifiedSignatureError):
+        with pytest.raises(UnverifiedSignatureError):
             key.verify_signature(sig, b"NOT DATA")
-
-
-# Run the unit tests.
-if __name__ == "__main__":
-    unittest.main()
