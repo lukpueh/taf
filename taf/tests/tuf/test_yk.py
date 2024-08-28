46017ccb2daf7b94043f3c60cf8a6e958777eb93
Lukas Puehringer <lukas.puehringer@nyu.edu>
2024-08-28 10:52:14 +0200

    Working YkSigner test with real Yubikey
    Removes pykcs11/softhsm setup (doesn't work with ykman)
    
    Test imports public key from connected yubikey and signs.
    Signing will prompt for a pin.
    
    ```
    REAL_YK=1 PYTEST_DISABLE_PLUGIN_AUTOLOAD=1 \
      pytest --noconftest taf/tests/tuf/ taf/tests/tuf/test_yk.py -s -v
    ```
    Signed-off-by: Lukas Puehringer <lukas.puehringer@nyu.edu>

--- taf/tests/tuf/test_yk.py
+++ taf/tests/tuf/test_yk.py
@@ -1,106 +1,33 @@
-"""Test HSMSigner"""
+"""Test YkSigner"""
 
 import os
 import pytest
 
-from PyKCS11 import PyKCS11
 from taf.tuf.keys import YkSigner
 
 from securesystemslib.exceptions import UnverifiedSignatureError
-from securesystemslib.signer import HSMSigner
-from securesystemslib.signer._hsm_signer import PYKCS11LIB
 
 
-_HSM_KEYID = 1
-_HSM_USER_PIN = "123456"
+class TestYkSigner:
+    """Test YkSigner"""
 
+    @pytest.mark.skipif(
+        not os.environ.get("REAL_YK"),
+        reason="Enable with REAL_YK env var (test prompts for pin)",
+    )
+    def test_real_yk(self):
+        """Test Yubikey public key export and signing with real Yubikey
+        NOTE: test will prompt for a pin.
+        """
+        from getpass import getpass
 
-def _generate_key_pair(session, keyid):
-    "Create rsa key pair on hsm"
-    public_template = [
-        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PUBLIC_KEY),
-        (PyKCS11.CKA_PRIVATE, PyKCS11.CK_FALSE),
-        (PyKCS11.CKA_MODULUS_BITS, 0x0400),
-        (PyKCS11.CKA_PUBLIC_EXPONENT, (0x01, 0x00, 0x01)),
-        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_ENCRYPT, PyKCS11.CK_FALSE),
-        (PyKCS11.CKA_VERIFY, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_WRAP, PyKCS11.CK_FALSE),
-        (PyKCS11.CKA_LABEL, str(keyid)),
-        (PyKCS11.CKA_ID, (keyid,)),
-    ]
-    private_template = [
-        (PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY),
-        (PyKCS11.CKA_TOKEN, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_SENSITIVE, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_DECRYPT, PyKCS11.CK_FALSE),
-        (PyKCS11.CKA_SIGN, PyKCS11.CK_TRUE),
-        (PyKCS11.CKA_UNWRAP, PyKCS11.CK_FALSE),
-        (PyKCS11.CKA_LABEL, str(keyid)),
-        (PyKCS11.CKA_ID, (keyid,)),
-    ]
-
-    session.generateKeyPair(public_template, private_template)
-
-
-@pytest.fixture
-def test_hsm(tmp_path):
-    """Initialize SoftHSM token and generate test keys"""
-    so_pin = "abcd"
-    token_label = "Test SoftHSM"
-
-    # Configure SoftHSM to create test token in temporary test directory
-    original_cwd = os.getcwd()
-    os.chdir(tmp_path)
-
-    with open("softhsm2.conf", "w", encoding="utf-8") as f:
-        f.write("directories.tokendir = " + str(tmp_path))
-    os.environ["SOFTHSM2_CONF"] = str(tmp_path / "softhsm2.conf")
-
-    # Only load shared library after above config
-    lib = PYKCS11LIB()
-    slot = lib.getSlotList(tokenPresent=True)[0]
-    lib.initToken(slot, so_pin, token_label)
-
-    session = PYKCS11LIB().openSession(slot, PyKCS11.CKF_RW_SESSION)
-    session.login(so_pin, PyKCS11.CKU_SO)
-    session.initPin(_HSM_USER_PIN)
-    session.logout()
-
-    session.login(_HSM_USER_PIN)
-
-    _generate_key_pair(session, _HSM_KEYID)
-
-    session.logout()
-    session.closeSession()
-
-    yield
-
-    os.chdir(original_cwd)
-    del os.environ["SOFTHSM2_CONF"]
-
-
-@pytest.mark.skipif(
-    not os.environ.get("PYKCS11LIB"), reason="set PYKCS11LIB to SoftHSM lib path"
-)
-class TestHSM:
-    """Test HSMSigner with SoftHSM
-
-    Requirements:
-    - install SoftHSM2
-    - set environment variable ``PYKCS11LIB`` to SoftHSM library path
-
-    See .github/workflows/hsm.yml for how this can be done on Linux, macOS and Windows.
-    """
-
-    def test_hsm(self, test_hsm):
-        """Test HSM key export and signing."""
+        def sec_handler(secret_name: str) -> str:
+            return getpass(f"Enter {secret_name}: ")
 
         key = YkSigner.import_()
-        print(key.to_dict())
-        # signer = HSMSigner(_HSM_KEYID, {}, key, lambda sec: _HSM_USER_PIN)
-        # sig = signer.sign(b"DATA")
-        # key.verify_signature(sig, b"DATA")
+        signer = YkSigner(key, sec_handler)
 
-        # with pytest.raises(UnverifiedSignatureError):
-        #     key.verify_signature(sig, b"NOT DATA")
+        sig = signer.sign(b"DATA")
+        key.verify_signature(sig, b"DATA")
+        with pytest.raises(UnverifiedSignatureError):
+            key.verify_signature(sig, b"NOT DATA")
